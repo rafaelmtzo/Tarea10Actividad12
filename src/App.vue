085653449ad31e2120ca52ee99cd3ef8e8907d62
rafaelmtzo <al03003870@tecmilenio.mx>
2024-04-21 10:43:30 -0600

    Actividad 12 y Tarea 10 version final

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+
+
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">Chuck Norris Jokes</h1>
+    <div class="row">
+      <div class="col-sm-12 col-md-4 mb-4" v-for="(joke, index) in chuckJokes" :key="index">
+        <chuck-card :icon-url="joke.icon_url" :value="joke.value"></chuck-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import ChuckCard from './components/ChuckCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ChuckCard
+  },
+  data() {
+    return {
+      chuckJokes: [
+        {
+          icon_url: "https://api.lorem.space/image/game?w=150&h=220",
+          value: "Chuck Norris can skydive into outer space."
+        },
+        {
+          icon_url: "https://api.lorem.space/image/album?w=150&h=150",
+          value: "The chief export of Chuck Norris is pain."
+        },
+        {
+          icon_url: "https://api.lorem.space/image/game?w=150&h=220",
+          value: "Chuck Norris doesn’t read books. He stares them down until he gets the information he wants."
+        },
+        {
+          icon_url: "https://api.lorem.space/image/album?w=150&h=150",
+          value: "Time waits for no man. Unless that man is Chuck Norris."
+        },
+        {
+          icon_url: "https://api.lorem.space/image/game?w=150&h=220",
+          value: "If you spell Chuck Norris in Scrabble, you win. Forever."
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  background: #f0f0f0;
+  margin: 10px 0;
+  padding: 10px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+
+h1 {
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ChuckCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card mb-3">
+    <img :src="iconUrl" class="card-img-top" alt="Chuck Norris">
+    <div class="card-body">
+      <p class="card-text">{{ value }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChuckCard',
+  props: ['iconUrl', 'value']
+}
+</script>
+
+<style scoped>
+.card {
+  /* Agrega sombra y bordes redondeados a la tarjeta */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 0.25rem;
+}
+
+.card-img-top {
+  height: 200px; /* o cualquier altura que prefieras */
+  object-fit: cover; /* Asegura que la imagen cubra todo el espacio disponible */
+  border-top-left-radius: calc(0.25rem - 1px);
+  border-top-right-radius: calc(0.25rem - 1px);
+}
+
+.card-body {
+  padding: 1rem;
+}
+
+.card-text {
+  margin: 0;
+}
+</style>
